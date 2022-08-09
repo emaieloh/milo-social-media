5510c1cc29f92bd43b59105f885a1a779221dc21
Milo Somera <milo.somera@gmail.com>
2022-08-09 20:40:56 +0800

    fix: card top margin

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -40,7 +40,7 @@ const Post = (props) => {
   );
 
   return (
-    <Card className="post-cards mx-auto">
+    <Card className="post-cards mx-auto mt-5">
       <div className="d-flex justify-content-between p-1">
         <span>{closeButton}</span>
         <span className="fst-italic smaller">{dateCreated}</span>
