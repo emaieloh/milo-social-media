59a955c92eb46d5c6fcebd5043852e57da7b05c4
Milo Somera <milo.somera@gmail.com>
2022-08-09 18:01:39 +0800

    Updates

---
+++ precache-manifest.529d1148c098042e46ef8d9d321526f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8a4c3d070a243ab04c4",
+    "url": "/milo-social-media/static/css/main.ebda0fea.chunk.css"
+  },
+  {
+    "revision": "a8a4c3d070a243ab04c4",
+    "url": "/milo-social-media/static/js/main.a8a4c3d0.chunk.js"
+  },
+  {
+    "revision": "f7eb4b1cc53c8035a687",
+    "url": "/milo-social-media/static/css/1.41c6d8ab.chunk.css"
+  },
+  {
+    "revision": "f7eb4b1cc53c8035a687",
+    "url": "/milo-social-media/static/js/1.f7eb4b1c.chunk.js"
+  },
+  {
+    "revision": "d61d2aab8519ba1cca15",
+    "url": "/milo-social-media/static/js/runtime~main.d61d2aab.js"
+  },
+  {
+    "revision": "49020f1f7817ea4a4ccb91540598e844",
+    "url": "/milo-social-media/index.html"
+  }
+];
